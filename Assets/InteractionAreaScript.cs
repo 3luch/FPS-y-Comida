0e1e5566d22a90de6015f10739f9648338250471
3luch <48366467@est.ort.edu.ar>
2024-06-10 15:57:50 -0300

    comida

---
+++ Assets/InteractionAreaScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionAreaScript : MonoBehaviour
+{
+
+
+    int puntaje = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PickUpScript>())
+        {
+            PickUpScript mante = other.GetComponent<PickUpScript>();
+            if (mante.esComida == true)
+            {
+                puntaje += mante.puntosHambre;
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                puntaje += mante.puntosHambre;
+
+            }
+        }
+    }
+}
